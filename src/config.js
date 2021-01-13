a1e395213ad69e1fde847e21624e414c9c36a28e
Hyunwoo Moon <mhw9163@gmail.com>
2021-01-13 08:36:52 -0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  FOLDER_ENDPOINT: 'http://localhost:8000/api/folders',
+  NOTE_ENDPOINT : 'http://localhost:8000/api/notes'
+}
